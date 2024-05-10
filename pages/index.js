cf16fe7e6192042ce96e4d320337f6557f8b508e
willianpauli <89708587+willianpauli@users.noreply.github.com>
2024-05-10 02:18:34 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Olá meu bem. É só uma mensagem encaminhada como exercicio do curso de programação. Me retorne um 'recebi', por favor.</h1>
+}
+
+export default Home;
