394dda11682633c2b6eff0c3201e4ea4fce6c863
willianpauli <89708587+willianpauli@users.noreply.github.com>
2024-05-14 23:16:25 +0000

    adiciona scripts `lint:check` e `lin:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # deschamps
+
 Curso de Programação com implementação do https://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Oi meu bem, este é um exercício do curso de programação. Pode me avisar que viu esta mensagem, por favor?</h1>
+  return (
+    <h1>
+      Oi meu bem, este é um exercício do curso de programação. Pode me avisar
+      que viu esta mensagem, por favor?
+    </h1>
+  );
 }
 
 export default Home;
